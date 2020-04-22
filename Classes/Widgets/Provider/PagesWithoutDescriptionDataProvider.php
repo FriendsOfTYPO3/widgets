83f7f52b061a71ff5322f0731ae1c460908135c1
Richard Haeser <richard@maxserv.com>
2020-04-22 20:02:03 +0200

    [WIP][TASK] Meta Description widget

--- Classes/Widgets/Provider/PagesWithoutDescriptionDataProvider.php
+++ Classes/Widgets/Provider/PagesWithoutDescriptionDataProvider.php
@@ -2,10 +2,33 @@
 declare(strict_types=1);
 namespace FriendsOfTYPO3\Widgets\Widgets\Provider;
 
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+
 class PagesWithoutDescriptionDataProvider implements PageProviderInterface
 {
+    /**
+     * @var UriBuilder
+     */
+    private $uriBuilder;
+
+    public function __construct(UriBuilder $uriBuilder)
+    {
+        $this->uriBuilder = $uriBuilder;
+    }
+
     public function getPages(): array
     {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+
+        $constraints = [
+            $queryBuilder->expr()->notIn('doktype', $this->excludedDoktypes),
+            $queryBuilder->expr()->eq('no_index', 0),
+            $queryBuilder->expr()->orX(
+                $queryBuilder->expr()->eq('description', $queryBuilder->createNamedParameter('')),
+                $queryBuilder->expr()->isNull('description')
+            ),
+        ];
+
         return [
             [
                 'uid' => 1,
